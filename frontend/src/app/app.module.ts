b6723ea180ebb6296eec13ea5a54a62e014e3b55
jaracodes <felipetk190@gmail.com>
2019-07-28 18:27:14 -0500

    Front end update : Routing has been created for the components, layout has also been incorporated

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LateralPanelComponent } from './components/lateral-panel/lateral-panel.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+/* Creating the router array*/ 
+
+const appRoutes : Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: '', component: LoginComponent, pathMatch: 'full'},  
+  /* wildcard*/
+  {path: '**', component: NotFoundComponent} 
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LateralPanelComponent,
+    FooterComponent,
+    LoginComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes , {enableTracing: true}),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/components/lateral-panel/lateral-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lateral-panel',
+  templateUrl: './lateral-panel.component.html',
+  styleUrls: ['./lateral-panel.component.css']
+})
+export class LateralPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
