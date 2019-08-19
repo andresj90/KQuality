e06187ce520d08ab6821f8d604c664dc71d9566f
DarkFACS <facs8813@gmail.com>
2019-08-19 15:09:17 -0500

    Operation correction of the SystemRole insert example in the link of the origination unit.

--- frontend/src/app/layout/organigram/organigram.component.ts
+++ frontend/src/app/layout/organigram/organigram.component.ts
@@ -1,3 +1,4 @@
+import { CompanyCRUDService } from './../../services/company-crud.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./organigram.component.scss']
 })
 export class OrganigramComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  /* variables */ 
+  newrole = {
+    name
   }
+  systemroles:any; 
+   
+  constructor(private companyService: CompanyCRUDService) { }
 
+    ngOnInit() {
+      this.companyService.listSystemRoles().subscribe(roles => {
+        this.systemroles = JSON.stringify(roles);
+        console.log(this.systemroles);
+      
+      });
+    }
+
+    getDataForCompanyRole(){
+      this.companyService.addNewSystemRole(this.newrole).subscribe(data=> {
+        console.log(data);
+      })
+    }
 }
