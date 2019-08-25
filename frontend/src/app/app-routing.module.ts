a1d5f7aa006263b22c1c90c9e0a4df0abe768385
jaracodes <felipetk190@gmail.com>
2019-08-24 21:07:01 -0500

    generated style for document create component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { DocumentMasterComponent } from './components/document-master/document-master.component';
+import { DocumentCreateComponent } from './components/document-create/document-create.component';
+
 
 const routes: Routes = [
   { path: '', component: LoginComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'create', component:  DocumentCreateComponent},
   /* wildcard*/
   { path: '**', component: NotFoundComponent },
 ];
@@ -16,4 +19,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { } export const 
-RoutingComponent = [LoginComponent,NotFoundComponent];
+RoutingComponent = [LoginComponent,NotFoundComponent, DocumentCreateComponent];
