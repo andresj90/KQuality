7f8fb78c00f9a159d8cd71d3ab0a48f2d85e4d7b
DarkFACS <facs8813@gmail.com>
2019-09-09 18:51:16 -0500

    fixes index

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ var http = require('http');
 var finalhandler = require('finalhandler');
 var serveStatic = require('serve-static');
 
-var serve = serveStatic("./frontend/dist/frontend");
+var serve = serveStatic("./frontend/dist/frontend/");
 
 var server = http.createServer(function(req, res) {
   var done = finalhandler(req, res);
   serve(req, res, done);
 });
 
+
+console.log('Lanzando servidor en: ' + process.env.PORT || 8000);
 server.listen(process.env.PORT || 8000);
