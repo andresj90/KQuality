67bf08ad511aa88f99adb2123eacaf9fce9b7553
DarkFACS <facs8813@gmail.com>
2019-09-09 19:27:47 -0500

    change directory

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var server = express();
-server.use('/', express.static(__dirname + '/'));
-server.use('/', express.static(__dirname + '/frontend/dist/frontend'));
+server.use('/', express.static(__dirname + '/production'));
 server.listen(process.env.PORT || 8000);
 console.log('Lanzando servidor en: ' + process.env.PORT || 8000);
