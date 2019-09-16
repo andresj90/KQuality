ad7cc7a0beb16bd655491f206fd761115973ae32
jaracodes <felipetk190@gmail.com>
2019-09-15 21:35:12 -0500

    final integration, angular and nodejs work

--- frontend/src/app/sharedModule/materialCustom.ts
+++ frontend/src/app/sharedModule/materialCustom.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatSelectModule, MatInputModule} from '@angular/material';
+import {MatStepperModule, MatInputModule, MatButtonModule, MatAutocompleteModule} from '@angular/material';
 import {MatRadioModule} from '@angular/material'
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatSelectModule} from '@angular/material/select';
+
 
 const materialModules = [
   BrowserAnimationsModule,
@@ -19,7 +22,10 @@ const materialModules = [
   MatMenuModule,
   MatSelectModule,
   MatInputModule,
-  MatProgressBarModule
+  MatProgressBarModule,
+  MatAutocompleteModule,
+  MatStepperModule,
+  MatExpansionModule
 ]
 
 @NgModule({
