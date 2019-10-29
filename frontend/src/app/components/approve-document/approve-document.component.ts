5d31c73a2cbffc84174566e0554210431aba0c8c
Diego Cortazar <cortazardiego@gmail.com>
2019-10-28 22:10:27 -0500

    Update approve-documents

--- frontend/src/app/components/approve-document/approve-document.component.ts
+++ frontend/src/app/components/approve-document/approve-document.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CRole } from './../../interfaces/croleinterface';
+import { DocumentCRUDService } from 'src/app/services/document-crud.service';
 
 @Component({
   selector: 'app-approve-document',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./approve-document.component.scss']
 })
 export class ApproveDocumentComponent implements OnInit {
+  isLinear = true;
+  panelOpenState = false;
 
-  constructor() { }
+  name: FormGroup;
+  description: FormGroup;
+  
+    /* properties from database */
+    documentos: CRole;
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private document: DocumentCRUDService 
+    ) { }
 
   ngOnInit() {
-  }
+    this.name = this._formBuilder.group({
+      name: ['', Validators.required]
+    });
+    this.description = this._formBuilder.group({
+      description: ['', Validators.required]
+    });
 
+    this.document.listAllDocuments().subscribe((data:CRole) =>{
+      this.documentos = data;
+      //console.log(this.documentos.elements);
+    });
+  }
 }
