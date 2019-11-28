b9f0c69be019ddba94d96eb98bbceee547614204
Evly <evelyny.guzmanc@konradlorenz.edu.co>
2019-11-27 23:28:38 -0500

    fix it

--- frontend/src/app/components/process/process.component.ts
+++ frontend/src/app/components/process/process.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient} from '@angular/common/http';
+import { FormControl, FormGroupDirective, NgForm } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-process',
@@ -6,10 +17,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./process.component.scss']
 })
 export class ProcessComponent implements OnInit {
+  
+  isLinear = false;
+  panelOpenState = false;
+  hide = true;
 
-  constructor() { }
+  newProcess = this._formBuilder.group({
+    name: ['', Validators.required],
+    prefix: ['', Validators.required],
+    description: ['', Validators.required],
+    type: ['', Validators.required]
+  }); 
+
+  constructor(
+    private http: HttpClient,
+    private _formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
   }
 
+  addProcess() {
+
+      this.hide = false;
+
+      let role = {
+        name: this.newProcess.get('name').value,
+        prefix: this.newProcess.get('prefix').value,
+        description: this.newProcess.get('description').value,        
+        type: this.newProcess.get('type').value 
+      } 
+
+    }
+
+
 }
+      
